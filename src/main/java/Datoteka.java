11b7da7aef73e7f9c5fc85ff96cd48c97b3106ef
Josip Bošnjak <jbosnjak@unipu.hr>
2019-02-02 13:31:11 +0100

    Dodane funkcije za parsiranje datoteke ispis vrijednosti mape i dohvat pozicije znaka jednakosti te funkcija za rastavljanje vrijednoti kao ključ vrijednost

--- src/main/java/Datoteka.java
+++ src/main/java/Datoteka.java
@@ -1,18 +1,25 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class Datoteka {
     private Locale lokalizacija;
     private Scanner ulazniTok;
     private FileInputStream ulaznaDatoteka;
+    private OsnovnaInformacija osnovnaInformacija;
 
     public Datoteka() throws FileNotFoundException {
         this.lokalizacija=new Locale("hr","HR");
         this.ulaznaDatoteka=new FileInputStream("properties.config");
         this.ulazniTok=new Scanner(ulaznaDatoteka,"UTF-8").useLocale(lokalizacija);
     }
+    public Datoteka(OsnovnaInformacija osnovnaInformacija) throws FileNotFoundException {
+        this.lokalizacija=new Locale("hr","HR");
+        this.ulaznaDatoteka=new FileInputStream("properties.config");
+        this.ulazniTok=new Scanner(ulaznaDatoteka,"UTF-8").useLocale(lokalizacija);
+        this.osnovnaInformacija=osnovnaInformacija;
+    }
+
 
     public Locale getLokalizacija() {
         return lokalizacija;
@@ -38,6 +45,14 @@ public class Datoteka {
         this.ulaznaDatoteka = ulaznaDatoteka;
     }
 
+    public OsnovnaInformacija getOsnovnaInformacija() {
+        return osnovnaInformacija;
+    }
+
+    public void setOsnovnaInformacija(OsnovnaInformacija osnovnaInformacija) {
+        this.osnovnaInformacija = osnovnaInformacija;
+    }
+
     public void ispisiDatoteku(){
         while (this.ulazniTok.hasNextLine()){
             String vrijednost1=ulazniTok.nextLine();
@@ -45,5 +60,46 @@ public class Datoteka {
         }
         this.ulazniTok.close();
     }
+    public void parsirajDatoteku(){
+        HashMap<String,String> mapa=new HashMap<String, String>();
+        while (this.ulazniTok.hasNextLine()){
+            String vrijednost=ulazniTok.nextLine();
+            mapa=rastaviKljucVrijednost(vrijednost);
+        }
+        System.out.println(mapa);
+    }
+    public HashMap<String,String> rastaviKljucVrijednost(String string){
+        HashMap<String,String> map=new HashMap<String,String>();
+        String kljuc="";
+        String vrijednost="";
+        int pozicijaStringa=0;
+        int pozicijaJednakosti=getPozicijuJednakosti(string);
+        for (int i=0;i<string.length();i++){
+            pozicijaStringa=i;
+            if(pozicijaStringa<pozicijaJednakosti){
+                kljuc+=string.charAt(i);
+            }else if(pozicijaStringa>pozicijaJednakosti){
+                vrijednost+=string.charAt(i);
+            }
 
+
+
+
+        }
+        map.put(kljuc,vrijednost);
+        return map;
+    }
+    public void ispisiVrijednostiMape(HashMap<String,String> mapa){
+        System.out.println(mapa);
+    }
+    public int getPozicijuJednakosti(String vrijednost){
+        int broj=0;
+        for (int i=0;i<vrijednost.length();i++){
+            if(vrijednost.charAt(i)=='='){
+                broj=i;
+                break;
+            }
+        }
+        return broj;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,14 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-         Datoteka datoteka=new Datoteka();
-         datoteka.ispisiDatoteku();
+        Datoteka datoteka=new Datoteka();
+        datoteka.parsirajDatoteku();
 
     }
 }
