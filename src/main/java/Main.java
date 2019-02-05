71382d92fa5112d90b01554be45e894cfb2a4230
Josip Bošnjak <jbosnjak@unipu.hr>
2019-02-05 10:33:59 +0100

    Svi se podaci ispisuju u pdf međutim potrebno je srediti layout detalis

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,6 +21,31 @@ public class Main {
         parameters.put("brojMobitela",informacija.getBrojMobitela());
         parameters.put("emailAdresa",informacija.getEmailAdresa());
         parameters.put("linkovi",informacija.getLinkovi());
+        parameters.put("zvanje",informacija.getZvanje());
+        parameters.put("datumPočetka",informacija.getDatumPočetka());
+        parameters.put("datumZavršetka",informacija.getDatumZavršetka());
+        parameters.put("titula",informacija.getTitula());
+        parameters.put("mjesto",informacija.getMjesto());
+        parameters.put("tehnikeZanimanja",informacija.getTehnikeZanimanja());
+        parameters.put("datumPočetka2",informacija.getDatumPocetka2());
+        parameters.put("datumKraja2",informacija.getDatumKraja2());
+        parameters.put("titula2",informacija.getTitula2());
+        parameters.put("mjesto2",informacija.getMjesto2());
+        parameters.put("tehnikeZanimanja2",informacija.getTehnikeZanimanja2());
+        parameters.put("ostaleVjestine",informacija.getOstaleVjestine());
+        parameters.put("materinskiJezik",informacija.getMaterinskiJezik());
+        parameters.put("straniJezici",informacija.getStraniJezici());
+        parameters.put("slušanje",informacija.getSlušanje());
+        parameters.put("čitanje",informacija.getČitanje());
+        parameters.put("govornaInterakcija",informacija.getGovornaInterakcija());
+        parameters.put("govornaProdukcija",informacija.getGovornaProdukcija());
+        parameters.put("pisanje",informacija.getPisanje());
+        parameters.put("vjestine",informacija.getVjestine());
+        parameters.put("obrada",informacija.getObrada());
+        parameters.put("komunikacija",informacija.getKomunikacija());
+        parameters.put("stvaranjeSadržaja",informacija.getStvaranjeSadržaja());
+        parameters.put("sigurnost",informacija.getSigurnost());
+        parameters.put("rješavanjeProblema",informacija.getRješavanjeProblema());
         try {
             printFileName =  JasperFillManager.fillReportToFile(
                     sourceFileName, parameters, beanColDataSource);
